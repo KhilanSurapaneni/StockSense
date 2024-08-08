41472f3958e506ada501d90ce321268f52dc4500
KhilanSurapaneni <khilans@gmail.com>
2024-08-08 13:51:57 +0530

    added comment

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from django.core.management import call_command
 
+# Define a Celery task that calls the update_data command
 @shared_task
 def update_data():
     call_command('update_data' , )

db3041383adbaacf73bd755da4a95d85b9ce96bb
KhilanSurapaneni <khilans@gmail.com>
2024-09-10 17:22:26 -0700

    removed frontend and added some ai

--- frontend/src/components/Landing/Description.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Description: React.FC = () => {
-  return (
-    <p className="text-center text-gray-400 mt-4 text-lg">
-      AI-driven ticker analysis with intuitive visualization.
-    </p>
-  );
-};
